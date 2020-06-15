b59b55d895cfc5bca24861e7ec43a6cb304bf335
Rochelle Ruiz <rcelevante@gmail.com>
2020-06-14 20:35:28 -0700

    questions and answers

--- script.js
+++ script.js
@@ -1,43 +1,130 @@
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 const quizQuestionElement = document.getElementById("code-quiz")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-buttons")
-
+const nextQuestionElement = document.getElementById("next-question")
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
 startButton.addEventListener("click", pad)
+answerButtonsElement.addEventListener("click", () => {
+    currentQuestionIndex++
+    //setNextQuestionElement()
+})
 
 function startGame() {
-    console.log("Started")
+    
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    quizQuestionElement.classList.remove('hide')
+   quizQuestionElement.classList.add('hide')
     setNextQuestion()
 }
 
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
+function showQuestion(questions) {
+    console.log(questions)
+    questionElement.innerText = questions.question
+    questions.answer.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+    nextQuestionElement.classList.remove("hide")
 }
 
-function selectAnswer() {
-
+function resetState () {
+    nextButton.classList.add("hide")
+    startButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
 }
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.lenght > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
 
 const questions = [
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answer: [
             { text: "javascript", correct: false },
-            { text: "terminal/bash", correct: false },
-            { text: "for loops", correct: true },
+            { text: "terminal/bash", correct: true },
+            { text: "for loops", correct: false },
             { text: "console.log", correct: false }
+        ]
+    },
+    {
+        question: "Arrays in Javascript can be used to store ________:",
+        answer: [
+            { text: "numbers and strings", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "all of the above", correct: true }
+        ]
+    },
+    {
+        questoion: "Commonly used data types Do NOT include ________:",
+        answer: [
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "alerts", correct: false },
+            { text: "numbers", correct: true }
+        ]
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ______:",
+        answer: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "square brackets", correct: false },
+            { text: "parenthesis", correct: true }
+        ]
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answer: [
+            { text: "commas", correct: false },
+            { text: "curely brackets", correct: false },
+            { text: "parenthesis", correct: false },
+            { text: "quotes", correct: true }
         ]
     }
 ]
